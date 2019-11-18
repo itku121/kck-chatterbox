a73f5a098f3a9866b9069f8e67b930bc0a14fd5f
itku121 <50325282+itku121@users.noreply.github.com>
2019-11-18 13:23:13 +0100

    Update views.py

--- chatterbox/chatterbox/views.py
+++ chatterbox/chatterbox/views.py
@@ -17,6 +17,32 @@ trainer = ChatterBotCorpusTrainer(chatbot)
 # chatbot.trainer.train('chatterbot.corpus.english')
 # chatbot.trainer.export_for_training('./export.yml')
 
-import gpt_2_simple as gpt2
 import os
 import requests
+
+@csrf_exempt
+def get_response(request):
+	response = {'status': None}
+	# f = open('blank.txt', 'r+')	
+	
+	
+	if request.method == 'POST':
+		data = json.loads(request.body.decode('utf-8'))
+		message = data['message']
+		print(message)
+		chat_response = chatbot.get_response(message).text
+		response['message'] = {'text': chat_response, 'user': False, 'chat_bot': True, 'username' : "Bot: "}
+		response['status'] = 'ok'
+
+	else:
+		response['error'] = 'no post data found'
+
+	return HttpResponse(
+		json.dumps(response),
+			content_type="application/json"
+		)
+
+
+def home(request, template_name="home.html"):
+	context = {'title': 'Chatbot Version 1.0'}
+	return render_to_response(template_name, context)
